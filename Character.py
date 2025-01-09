4a2e7ca390e3bfb9990751ba8abcee311b467346
m.lieval <m.lieval@student.isartdigital.com>
2025-01-09 15:26:22 +0100

    Ask Gameloop & ASCII art implemented

--- Character.py
+++ Character.py
@@ -1,12 +1,13 @@
 class Character:
-    def __init__(self,name,hp,level,xp,strength,dexterity,constitution,intelligence,wisdom,charisma):
+    def __init__(self,name,hp,level,xp,gold,strength,dexterity,constitution,intelligence,wisdom,charisma):
         self.name = name
         self.hp = hp
         self.level = level
         self.xp = xp
-        self.strength = strength
-        self.dexterity = dexterity
-        self.constitution = constitution
-        self.intelligence = intelligence
-        self.wisdom = wisdom
-        self.charisma = charisma
+        self.gold = gold
+        self.strength_bonus = strength
+        self.dexterity_bonus = dexterity
+        self.constitution_bonus = constitution
+        self.intelligence_bonus = intelligence
+        self.wisdom_bonus = wisdom
+        self.charisma_bonus = charisma
